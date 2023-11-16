29b89c6b7e0a67b6e4be058095e385672c0b2b6e
Rarstyak <rarstyak@gmail.com>
2023-11-16 14:00:52 -0500

    add standard pawn threat

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -6,22 +6,38 @@ require_relative 'piece'
 class Pawn < Piece
   PREFIX = ''
 
-  # def get_threat(board, coor)
-  #   list = []
-  #   if @color == :white
-  #     # only threaten rel [[1, 1], [-1, 1]] if enemy present
-  #     # 'threaten' to move forward if no piece
-  #     # 'threaten' to move forward twice if no piece in either
-  #     # ??? kill en passant
-  #   end
-  #   if @color == :black
-  #     # only threaten rel [[1, -1], [-1, -1]] if enemy present
-  #     # 'threaten' to move forward if no piece
-  #     # 'threaten' to move forward twice if no piece in either
-  #     # ??? kill en passant
-  #   end
-  #   list.compact
-  # end
+  def get_threat(board, coor)
+    list = []
+    if @color == :white
+      # only threaten rel [[1, 1], [-1, 1]] if enemy present
+      rel_cell_r = board.get_cell(coor, [1, 1])
+      list.push(rel_cell_r&.coor) unless rel_cell_r&.piece.nil? or same_color?(rel_cell_r&.piece)
+      rel_cell_l = board.get_cell(coor, [-1, 1])
+      list.push(rel_cell_l&.coor) unless rel_cell_l&.piece.nil? or same_color?(rel_cell_l&.piece)
+      # 'threaten' to move forward if no piece
+      rel_cell_f = board.get_cell(coor, [0, 1])
+      list.push(rel_cell_f&.coor) if rel_cell_f&.piece.nil?
+      # 'threaten' to move forward twice if no piece in either and coor[1] == 1
+      rel_cell_ff = board.get_cell(coor, [0, 2])
+      list.push(rel_cell_ff&.coor) if rel_cell_f&.piece.nil? and rel_cell_ff&.piece.nil? and coor[1] == 1
+      # ??? kill en passant
+    end
+    if @color == :black
+      # only threaten rel [[1, -1], [-1, -1]] if enemy present
+      rel_cell_r = board.get_cell(coor, [1, -1])
+      list.push(rel_cell_r&.coor) unless rel_cell_r&.piece.nil? or same_color?(rel_cell_r&.piece)
+      rel_cell_l = board.get_cell(coor, [-1, -1])
+      list.push(rel_cell_l&.coor) unless rel_cell_l&.piece.nil? or same_color?(rel_cell_l&.piece)
+      # 'threaten' to move forward if no piece
+      rel_cell_f = board.get_cell(coor, [0, -1])
+      list.push(rel_cell_f&.coor) if rel_cell_f&.piece.nil?
+      # 'threaten' to move forward twice if no piece in either and coor[1] == 6
+      rel_cell_ff = board.get_cell(coor, [0, -2])
+      list.push(rel_cell_ff&.coor) if rel_cell_f&.piece.nil? and rel_cell_ff&.piece.nil? and coor[1] == 6
+      # ??? kill en passant
+    end
+    list.compact
+  end
 
   def to_s
     @color == :white ? white_pawn : black_pawn
--- main.rb
+++ main.rb
@@ -18,6 +18,7 @@ pawn_white = Pawn.new(:white)
 pawn_black = Pawn.new(:black)
 knight_white = Knight.new(:white)
 knight_black = Knight.new(:black)
+queen_white = Queen.new(:white)
 
 puts pawn_white
 var = pawn_white.class.name
@@ -33,64 +34,21 @@ p board.get_piece([0,0])
 p board.get_piece([0,0], [1,1])
 
 puts "TESTING TIME"
-knight = Knight.new(:white)
 blank = Board.new(0, [], [])
+
+blank.coor_place([0, 1], knight_white)
+blank.coor_place([2, 2], pawn_white)
+blank.coor_place([3, 3], pawn_black)
+
 blank.print_board
+
 p blank.list_pieces
-knight.get_threat(blank, [4,4]).each do |coor|
-  blank.coor_place(coor, pawn_white)
-end
-blank.coor_place([5,6], pawn_black)
+p pawn_white.get_threat(blank, [2, 2])
 
-
-blank.print_board
-
-p knight.get_threat(blank, [4,4])
-
-puts "TESTING TIME Rook"
-rook = Rook.new(:white)
-blank = Board.new(0, [], [])
-blank.coor_place([4,6], pawn_black)
-blank.coor_place([4,1], knight_white)
-blank.print_board
-p blank.list_pieces
-rook.get_threat(blank, [4,4]).each do |coor|
-  blank.coor_place(coor, pawn_white)
+pawn_white.get_threat(blank, [2, 2]).each do |coor|
+  blank.coor_place(coor, queen_white)
 end
 
-
 blank.print_board
 
-p rook.get_threat(blank, [4,4])
-
-puts "TESTING TIME Bishop"
-bishop = Bishop.new(:white)
-blank = Board.new(0, [], [])
-blank.coor_place([4,6], pawn_black)
-blank.coor_place([5,1], knight_white)
-blank.print_board
-p blank.list_pieces
-bishop.get_threat(blank, [2,4]).each do |coor|
-  blank.coor_place(coor, pawn_white)
-end
-
-
-blank.print_board
-
-p bishop.get_threat(blank, [4,4])
-
-puts "TESTING TIME Queen"
-queen = Queen.new(:white)
-blank = Board.new(0, [], [])
-blank.coor_place([4,6], pawn_black)
-blank.coor_place([4,1], knight_white)
-blank.print_board
-p blank.list_pieces
-queen.get_threat(blank, [4,4]).each do |coor|
-  blank.coor_place(coor, pawn_white)
-end
-
-
-blank.print_board
-
-p queen.get_threat(blank, [4,4])
+p pawn_white.get_threat(blank, [2, 2])
